3e1d3a14bc89af0e2bfacba1e92ec6a666808281
Lars Boegild Thomsen <lth@netcompartner.com>
2024-02-28 10:58:48 +0800

    First commit

---
+++ src/ld2410.c
@@ -0,0 +1,21 @@
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 Lars Boegild Thomsen <lbthomsen@gmail.com>
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+#include "main.h"
+#include "ld2410.h"
+
---
+++ src/ld2410.h
@@ -0,0 +1,24 @@
+/**
+  ******************************************************************************
+  * @file           : ld2410.h
+  * @brief          : Header for ld2410 library
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 Lars Boegild Thomsen <lbthomsen@gmail.com>
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+#ifndef LD2410_H_
+#define LD2410_H_
+
+
+
+#endif /* LD2410_H_ */
